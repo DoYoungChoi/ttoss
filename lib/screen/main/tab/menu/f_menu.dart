204b287ebde1be068a2caa1ca71f400b0329e83a
dodor <choidy9298@naver.com>
2024-05-09 10:46:55 +0900

    update: Bottom Tabbar 수정

---
+++ lib/screen/main/tab/menu/f_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MenuFragment extends StatefulWidget {
+  const MenuFragment({super.key});
+
+  @override
+  State<MenuFragment> createState() => _MenuFragmentState();
+}
+
+class _MenuFragmentState extends State<MenuFragment> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
